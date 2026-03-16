6c922363453f4d41b96b0627a4319a15551b6786
Duaneo128456 <115256264+Duaneo128456@users.noreply.github.com>
2026-03-16 21:37:39 +0000

    Add picom and xterm to installation script

--- install_i3_tools.sh
+++ install_i3_tools.sh
@@ -29,7 +29,7 @@ echo "#   pcmanfm, gdebi, btop (terminal system monitor), and    #"
 echo "#   fastfetch (display system configuration)               #"
 echo "############################################################"
 
-sudo apt install gdebi l3afpad firefox-esr pcmanfm btop fastfetch -y
+sudo apt install gdebi l3afpad firefox-esr pcmanfm btop fastfetch picom xterm -y
 
 echo "#############################################################"
 echo "#   Please now reboot your system and chose "win" as your   #"
