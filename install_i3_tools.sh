cbd56ec664c7d4e22ba45cafeb68460c3462e3ce
Duaneo128456 <115256264+Duaneo128456@users.noreply.github.com>
2026-03-19 21:57:03 +0000

    Refactor i3-wm installation script with user prompts
    Updated the installation script for i3-wm to include a welcome message and user prompts. Enhanced the script's clarity and structure.

--- install_i3_tools.sh
+++ install_i3_tools.sh
@@ -1,33 +1,41 @@
 #! /bin/bash
 
-echo "#########################################################"
-echo "#        Installing the basics for an i3-wm setup       #"
-echo "#########################################################"
-echo ""
-echo "" 
+echo "  ######    #######     #####  #####  #####      ###      ###     "
+echo "  #////#    #//////#    #///#  #///#  #///#     #///#    #///#    "
+echo "   #//#     ####////#   #///#  #///#  #///#    #/////#  #/////#   "
+echo "   #//#         #////#  #///#  #///#  #///#   #///#///##///#///#  "
+echo "   #//#     ####////#   #///#  #///#  #///#  #///# #/////#  #///# "
+echo "   #//#     #//////#    #///#  #///#  #///#  #///#  #///#   #///# "
+echo "   #//#     ####////#   #///# #/////# #///#  #///#  #///#   #///# "
+echo "   #//#         #////#  #///#///##///#///#   #///#  #///#   #///# "
+echo "   #//#     ####////#    #/////#  #/////#    #///#  #///#   #///# "
+echo "  #////#    #//////#      #///#    #///#     #///#  #///#   #///# "
+echo "  ######    ######         ##       ##       #####  #####   ##### "
 
-echo "#########################################################"
-echo "#           Installing base install of i3-wm            #"
-echo "#########################################################"
+
+echo "Welcome to my setup script for I3-WM"
+echo "This script will setup a basic Debian 13 (Trixie) install of I3 Window Manager"
+echo "This is a personal project (Mainly for myself to be able to setup a basic I3-WM system)"
+echo "but if anyone else decides to use this then I am greatful for your testing and support."
+echo "I will not be adding any new features to this just essential updates"
+echo "Please type "continue" to start this script"
+echo "If you wish to cancel this script without running it then type "cancel""
+
+read varcon
+
+if [ $varcon = continue ]
+then
+# this installs base i3 system
 
 sudo apt install i3 suckless-tools xorg lightdm -y
 
-echo ""
-
-echo "###########################################################"
-echo "#   Installing network manager applet, screenshot tool,   #" 
-echo "#   copy and paste functionality, brightness controls.    #"
-echo "###########################################################"
+# This installs: network manager applet, screenshot capability, copy and paste capability,
+# screen brightness controls and sound controls
 
 sudo apt install network-manager-applet scrot diodon brightnessctl alsa-utils -y
 
-echo ""
-
-echo "############################################################"
-echo "#   Installing basic apps such as: firefox-esr, l3afpad,   #" 
-echo "#   pcmanfm, gdebi, btop (terminal system monitor), and    #"
-echo "#   fastfetch (display system configuration)               #"
-echo "############################################################"
+# This installs basic apps such as: firefox-esr, l3afpad, pcmanfm, gdebi, 
+# btop (terminal system monitor), fastfetch (display system configuration)               
 
 sudo apt install gdebi l3afpad firefox-esr pcmanfm btop fastfetch picom xterm arandr lxappearance feh -y
 
@@ -43,3 +51,12 @@ echo "~   WARNING!!!:                                             ~"
 echo "~   Without using my configuration file then parts of this  ~"
 echo "~   install will not function as intended.                  ~"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+fi
+
+if [ $varcon = cancel ]
+then
+echo "#############################################################"
+echo "# No changes have been made to your system, if you wish     #"
+echo "# to run this script then please rerun this script          #"
+echo "#############################################################"
+fi
